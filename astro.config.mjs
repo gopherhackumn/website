f4becd0fc791fe9a101d63ae4a4db9ab098ccc3c
Michael Zhang <mail@mzhang.io>
2024-12-07 22:51:16 -0600

    staging

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import yaml from '@rollup/plugin-yaml';
 
 // https://astro.build/config
 export default defineConfig({
+    base: process.env.BASE ?? "/",
     integrations: [mdx()],
     vite: {
         plugins: [yaml()]
