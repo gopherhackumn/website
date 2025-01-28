ab3211733784c40f34e1c2b82796afb0aa4bb4ee
Michael Zhang <mail@mzhang.io>
2025-01-28 17:08:58 -0600

    add author association

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,36 +2,37 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "src/content/blog",
-  }),
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    author: z.string().or(z.string().array()),
-    draft: z.boolean().optional(),
-    tags: z.string().array().optional(),
-  }),
+	loader: glob({
+		pattern: "**/*.{md,mdx}",
+		base: "src/content/blog",
+	}),
+	schema: z.object({
+		title: z.string(),
+		date: z.date(),
+		author: z.string().or(z.string().array()),
+		draft: z.boolean().optional(),
+		tags: z.string().array().optional(),
+		ctf_id: z.string().optional(),
+	}),
 });
 
 const events = defineCollection({
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "src/content/events",
-  }),
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-  }),
+	loader: glob({
+		pattern: "**/*.{md,mdx}",
+		base: "src/content/events",
+	}),
+	schema: z.object({
+		title: z.string(),
+		date: z.date(),
+	}),
 });
 
 const pages = defineCollection({
-  loader: glob({
-    pattern: "**/*.{md,mdx}",
-    base: "src/content/pages",
-  }),
-  schema: z.object({ title: z.string() }),
+	loader: glob({
+		pattern: "**/*.{md,mdx}",
+		base: "src/content/pages",
+	}),
+	schema: z.object({ title: z.string() }),
 });
 
 export const collections = { blog, events, pages };
